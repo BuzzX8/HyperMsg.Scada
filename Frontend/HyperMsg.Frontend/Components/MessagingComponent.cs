924f0e9e4d7c8d7e334fa7ef0ce56aa875735a4c
BuzzX8 <maxim.chasnik@gmail.com>
2025-07-27 19:10:38 +0300

    wip

--- Frontend/HyperMsg.Frontend/Components/MessagingComponent.cs
+++ Frontend/HyperMsg.Frontend/Components/MessagingComponent.cs
@@ -1,16 +1,32 @@
 ﻿using HyperMsg.Messaging;
+using HyperMsg.Scada.Frontend.Shared.Messages;
+using HyperMsg.Scada.Frontend.Shared.Models;
 
 namespace HyperMsg.Frontend.Components;
 
 public class MessagingComponent : IMessagingComponent
 {
+    private readonly List<IDisposable> registrations = [];
+
     public void Attach(IMessagingContext messagingContext)
     {
-        throw new NotImplementedException();
+        messagingContext.HandlerRegistry.RegisterRequestHandler<DeviceListRequest, DeviceListResponse>(HandleDeviceListRequestAsync);
+        //registrations.Add(regisatration);
     }
 
     public void Detach(IMessagingContext messagingContext)
     {
-        throw new NotImplementedException();
+        
+    }
+
+    private Task<DeviceListResponse> HandleDeviceListRequestAsync(DeviceListRequest request, CancellationToken cancellationToken)
+    {
+        // Handle the device list request here
+        // For example, you might fetch the list of devices and send a response back
+        return Task.FromResult(new DeviceListResponse(new List<Device>
+            {
+                new() { Id = "1", Name = "Device 1" },
+                new() { Id = "2", Name = "Device 2" }
+            }));        
     }
 }

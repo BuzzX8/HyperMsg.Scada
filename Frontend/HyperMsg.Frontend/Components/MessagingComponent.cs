55b23987e24298a8c84fe5aec2616a935ac4cb71
BuzzX8 <maxim.chasnik@gmail.com>
2025-07-27 21:58:14 +0300

    wip

--- Frontend/HyperMsg.Frontend/Components/MessagingComponent.cs
+++ Frontend/HyperMsg.Frontend/Components/MessagingComponent.cs
@@ -10,23 +10,27 @@ public class MessagingComponent : IMessagingComponent
 
     public void Attach(IMessagingContext messagingContext)
     {
-        messagingContext.HandlerRegistry.RegisterRequestHandler<DeviceListRequest, DeviceListResponse>(HandleDeviceListRequestAsync);
-        //registrations.Add(regisatration);
+        var registration = messagingContext.HandlerRegistry.RegisterRequestHandler<DeviceListRequest, DeviceListResponse>(HandleDeviceListRequestAsync);
+        registrations.Add(registration);
     }
 
-    public void Detach(IMessagingContext messagingContext)
+    public void Detach(IMessagingContext _)
     {
-        
+        foreach (var registration in registrations)
+        {
+            registration.Dispose();
+        }
+        registrations.Clear();
     }
 
     private Task<DeviceListResponse> HandleDeviceListRequestAsync(DeviceListRequest request, CancellationToken cancellationToken)
     {
         // Handle the device list request here
         // For example, you might fetch the list of devices and send a response back
-        return Task.FromResult(new DeviceListResponse(new List<Device>
-            {
-                new() { Id = "1", Name = "Device 1" },
-                new() { Id = "2", Name = "Device 2" }
-            }));        
+        return Task.FromResult(new DeviceListResponse(
+            [
+                new() { Id = "1", Name = "Device 1", Status = "Online", Type = "EdgeNode" },
+                new() { Id = "2", Name = "Device 2", Status = "Offline", Type = "Sensor" }
+            ]));        
     }
 }

0f4d2b3ac7185d8e7adfbc417e1fcfb5d04c5e32
Maxim <maxim.chasnik@outlook.com>
2025-07-29 14:17:11 +0300

    wip

---
+++ Shared/HyperMsg.Scada.Shared/Messages/DispatcherExtensions.cs
@@ -0,0 +1,21 @@
+﻿using HyperMsg.Messaging;
+using HyperMsg.Scada.Shared.Models;
+
+namespace HyperMsg.Scada.Shared.Messages;
+
+public static class DispatcherExtensions
+{
+    public static IEnumerable<Device> DispatchDeviceListRequest(this IDispatcher dispatcher)
+    {
+        var response = dispatcher.DispatchRequest<DeviceListRequest, DeviceListResponse>(new());
+
+        return response?.Devices ?? [];
+    }
+
+    public static async Task<IEnumerable<Device>> DispatchDeviceListRequestAsync(this IDispatcher dispatcher, CancellationToken cancellationToken)
+    {
+        var response = await dispatcher.DispatchRequestAsync<DeviceListRequest, DeviceListResponse>(new(), cancellationToken);
+
+        return response?.Devices ?? [];
+    }
+}
---
+++ Shared/HyperMsg.Scada.Shared/Messages/HandlerRegistryExtensions.cs
@@ -0,0 +1,20 @@
+﻿using HyperMsg.Messaging;
+
+namespace HyperMsg.Scada.Shared.Messages;
+
+public static class HandlerRegistryExtensions
+{
+    public static IDisposable RegisterDeviceListRequestHandler(
+        this IHandlerRegistry handlersRegistry,
+        RequestHandler<DeviceListRequest, DeviceListResponse> handler)
+    {
+        return handlersRegistry.RegisterRequestHandler(handler);
+    }
+
+    public static IDisposable RegisterDeviceListRequestHandler(
+        this IHandlerRegistry handlersRegistry,
+        AsyncRequestHandler<DeviceListRequest, DeviceListResponse> handler)
+    {
+        return handlersRegistry.RegisterRequestHandler(handler);
+    }
+}

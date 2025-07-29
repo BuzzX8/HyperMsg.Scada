249fcadf88b29b907d830656cb9f36b9f23ba145
Maxim <maxim.chasnik@outlook.com>
2025-07-29 13:39:58 +0300

    wip

--- Backend/HyperMsg.Scada.WebApi/Controllers/DeviceController.cs
+++ Backend/HyperMsg.Scada.WebApi/Controllers/DeviceController.cs
@@ -1,39 +1,81 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using HyperMsg.Messaging;
 using Microsoft.AspNetCore.Mvc;
 
-namespace HyperMsg.Scada.WebApi.Controllers
+namespace HyperMsg.Scada.WebApi.Controllers;
+
+/// <summary>
+/// API for managing devices.
+/// </summary>
+[ApiController]
+[Route("api/[controller]")]
+public class DeviceController : ControllerBase
 {
-    [Route("api/[controller]")]
-    public class DeviceController : ControllerBase
+    private readonly IMessagingContext _messagingContext;
+    private readonly ILogger<DeviceController> _logger;
+
+    public DeviceController(IMessagingContext messagingContext, ILogger<DeviceController> logger)
     {
-        [HttpGet]
-        public IActionResult GetAll()
-        {
-            return Ok();
-        }
+        _messagingContext = messagingContext;
+        _logger = logger;
+    }
 
-        [HttpGet("{deviceId}")]
-        public IActionResult GetById(string deviceId)
-        {
-            return Ok();
-        }
+    /// <summary>
+    /// Gets all devices.
+    /// </summary>
+    /// <returns>List of devices.</returns>
+    [HttpGet]
+    [EndpointDescription("Get all devices")]
+    public IActionResult GetAll()
+    {
+        return Ok();
+    }
 
-        [HttpPost]
-        public IActionResult Create([FromBody]object device)
-        {
-            return CreatedAtAction(nameof(GetById), new { deviceId = "newDeviceId" }, device);
-        }
+    /// <summary>
+    /// Gets a device by its ID.
+    /// </summary>
+    /// <param name="deviceId">The ID of the device.</param>
+    /// <returns>The requested device.</returns>
+    [HttpGet("{deviceId}")]
+    [EndpointDescription("Get device by ID")]
+    public IActionResult GetById(string deviceId)
+    {
+        return Ok();
+    }
 
-        [HttpPut("{deviceId}")]
-        public IActionResult Edit(string deviceId, [FromBody]object device)
-        {
-            return NoContent();
-        }
+    /// <summary>
+    /// Creates a new device.
+    /// </summary>
+    /// <param name="device">The device to create.</param>
+    /// <returns>The created device.</returns>
+    [HttpPost]
+    [EndpointDescription("Create a new device")]
+    public IActionResult Create([FromBody]object device)
+    {
+        return CreatedAtAction(nameof(GetById), new { deviceId = "newDeviceId" }, device);
+    }
 
-        [HttpDelete("{deviceId}")]
-        public ActionResult Delete(string deviceId)
-        {
-            return NoContent();
-        }
+    /// <summary>
+    /// Updates an existing device.
+    /// </summary>
+    /// <param name="deviceId">The ID of the device to update.</param>
+    /// <param name="device">The updated device data.</param>
+    /// <returns>No content.</returns>
+    [HttpPut("{deviceId}")]
+    [EndpointDescription("Update an existing device")]
+    public IActionResult Edit(string deviceId, [FromBody]object device)
+    {
+        return NoContent();
+    }
+
+    /// <summary>
+    /// Deletes a device by its ID.
+    /// </summary>
+    /// <param name="deviceId">The ID of the device to delete.</param>
+    /// <returns>No content.</returns>
+    [HttpDelete("{deviceId}")]
+    [EndpointDescription("Delete a device by ID")]
+    public ActionResult Delete(string deviceId)
+    {
+        return NoContent();
     }
 }

79114b6a7d000fb277241480fd12cbce562e604a
Max Gunther <maxim.chasnik@gmail.com>
2025-07-29 10:31:37 +0300

    Device Controller (#4)

---
+++ Backend/HyperMsg.Scada.WebApi/Controllers/DeviceController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HyperMsg.Scada.WebApi.Controllers
+{
+    public class DeviceController : Controller
+    {
+        // GET: DeviceController
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: DeviceController/5
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // POST: DeviceController
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // POST: DeviceController/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // DELETE: DeviceController/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+    }
+}

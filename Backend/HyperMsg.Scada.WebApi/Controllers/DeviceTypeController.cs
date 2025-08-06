cefea3a27b4d6fcb12b4e7442f5b7d5522176e24
Maxim <maxim.chasnik@gmail.com>
2025-08-06 10:27:33 +0300

    wip

---
+++ Backend/HyperMsg.Scada.WebApi/Controllers/DeviceTypeController.cs
@@ -0,0 +1,31 @@
+﻿using HyperMsg.Messaging;
+using HyperMsg.Scada.Shared.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HyperMsg.Scada.WebApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class DeviceTypeController : ControllerBase
+{
+    private readonly IDispatcher _dispatcher;
+    private readonly ILogger<DeviceTypeController> _logger;
+    public DeviceTypeController(IDispatcher dispatcher, ILogger<DeviceTypeController> logger)
+    {
+        _dispatcher = dispatcher;
+        _logger = logger;
+    }
+    /// <summary>
+    /// Gets all device types.
+    /// </summary>
+    /// <returns>List of device types.</returns>
+    [HttpGet]
+    [EndpointDescription("Get all device types")]
+    [ProducesResponseType(typeof(IEnumerable<DeviceType>), StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetAll(CancellationToken cancellationToken)
+    {
+        //var response = await _dispatcher.DispatchDeviceTypeListRequestAsync(cancellationToken);
+        return Ok();// response);
+    }
+}
---
+++ Shared/HyperMsg.Scada.Shared/Models/DeviceType.cs
@@ -0,0 +1,17 @@
+﻿namespace HyperMsg.Scada.Shared.Models;
+
+public class DeviceType
+{
+    /// <summary>
+    /// Gets or sets the unique identifier of the device type.
+    /// </summary>
+    public string Id { get; set; } = string.Empty;
+    /// <summary>
+    /// Gets or sets the name of the device type.
+    /// </summary>
+    public string Name { get; set; } = string.Empty;
+    /// <summary>
+    /// Gets or sets the description of the device type.
+    /// </summary>
+    public string Description { get; set; } = string.Empty;
+}

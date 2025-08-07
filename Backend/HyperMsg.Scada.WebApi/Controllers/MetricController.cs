9cb1e172ac8df883b613af97605a6dabd8ac6020
Maxim <maxim.chasnik@gmail.com>
2025-08-07 12:48:36 +0300

    wip

---
+++ Backend/HyperMsg.Scada.WebApi/Controllers/MetricController.cs
@@ -0,0 +1,30 @@
+﻿using HyperMsg.Messaging;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HyperMsg.Scada.WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MetricController : ControllerBase
+{
+    private readonly IDispatcher _dispatcher;
+    private readonly ILogger<MetricController> _logger;
+
+    public MetricController(IDispatcher dispatcher, ILogger<MetricController> logger)
+    {
+        _dispatcher = dispatcher;
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetMetricsAsync([FromQuery] DateTime? from, [FromQuery] DateTime? to, CancellationToken cancellationToken)
+    {        
+        return Ok();
+    }
+
+    [HttpGet("{deviceId}")]
+    public async Task<IActionResult> GetMetricsByDeviceIdAsync(string deviceId, [FromQuery] DateTime? from, [FromQuery] DateTime? to)
+    {
+        return Ok();
+    }
+}
---
+++ Shared/HyperMsg.Scada.Shared/Models/DeviceMetric.cs
@@ -0,0 +1,3 @@
+﻿namespace HyperMsg.Scada.Shared.Models;
+
+public record DeviceMetric(string DeviceId, DateTime Timestamp, object Payload);

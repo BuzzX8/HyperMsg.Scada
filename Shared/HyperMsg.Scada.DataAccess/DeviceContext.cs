da2ea7764a84e887cdcef2a792c7c6c6904442ba
BuzzX8 <maxim.chasnik@gmail.com>
2025-08-17 15:38:40 +0300

    wip

---
+++ Shared/HyperMsg.Scada.DataAccess/DeviceContext.cs
@@ -0,0 +1,19 @@
+﻿using HyperMsg.Scada.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HyperMsg.Scada.DataAccess;
+
+public class DeviceContext(DbContextOptions<DeviceContext> options) : DbContext(options)
+{
+    public DbSet<Device> Devices { get; set; }
+
+    public Task<Device?> GetDeviceByIdAsync(string id)
+    {
+        return Devices.FirstOrDefaultAsync(d => d.Id == id);
+    }
+
+    public IAsyncEnumerable<Device> GetAllDevicesAsync()
+    {
+        return Devices.AsAsyncEnumerable();
+    }
+}
---
+++ Shared/HyperMsg.Scada.DataAccess/DeviceTypeContext.cs
@@ -0,0 +1,21 @@
+﻿using HyperMsg.Scada.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HyperMsg.Scada.DataAccess;
+
+public class DeviceTypeContext(DbContextOptions<DeviceTypeContext> options) : DbContext(options)
+{
+    public DbSet<DeviceType> DeviceTypes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<DeviceType>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Id).IsRequired().HasMaxLength(50);
+            entity.Property(e => e.Name).IsRequired().HasMaxLength(100);
+            entity.Property(e => e.Description).HasMaxLength(500);
+            entity.HasIndex(e => e.Name).IsUnique();
+        });
+    }
+}

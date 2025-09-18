14939800ab2ea87c29a52cb544efb7429a1a5b40
BuzzX8 <maxim.chasnik@gmail.com>
2025-09-18 19:35:03 +0300

    wip

--- Backend/HyperMsg.Scada.WebApi/Program.cs
+++ Backend/HyperMsg.Scada.WebApi/Program.cs
@@ -2,50 +2,59 @@ using HyperMsg.Messaging;
 using HyperMsg.Scada.DataAccess;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-//var connectionString = builder.Configuration.GetConnectionString("LocalMsSql") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-
-builder.Services.AddDbContext<UserContext>(options => options.UseSqlServer(connectionString));
-builder.Services.AddIdentityApiEndpoints<IdentityUser>()
-    .AddEntityFrameworkStores<UserContext>();
-
-// Add services to the container.
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddMessagingContext();
-
-builder.Services.AddDataComponent();
-builder.Services.AddDataAccessRepositories(options =>
-{
-    options.UseSqlServer(connectionString);
-});
+AddServices(builder.Services, builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.5
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-    app.MapIdentityApi<IdentityUser>();
-}
-else
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-//app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+ConfigureApplication(app);
 
 app.Run();
+
+static void AddServices(IServiceCollection services, IConfiguration configuration)
+{
+    var connectionString = configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+    //var connectionString = configuration.GetConnectionString("LocalMsSql") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+    services.AddDbContext<UserContext>(options => options.UseSqlite());
+    services.AddIdentityApiEndpoints<IdentityUser>()
+        .AddEntityFrameworkStores<UserContext>();
+
+    // Add services to the container.
+    services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    services.AddEndpointsApiExplorer();
+    services.AddSwaggerGen();
+    services.AddMessagingContext();
+
+    services.AddDataComponent();
+    services.AddDataAccessRepositories(options =>
+    {
+        options.UseSqlServer(connectionString);
+    });
+}
+
+static void ConfigureApplication(WebApplication app)
+{
+    // Configure the HTTP request pipeline.5
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+        app.MapIdentityApi<IdentityUser>();
+    }
+    else
+    {
+        app.UseExceptionHandler("/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    //app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+}

058ec7f0fe13b7102f064beef13ef725f3b970f6
Max Gunther <maxim.chasnik@gmail.com>
2025-10-20 11:04:09 +0300

    wip

---
+++ Shared/HyperMsg.Scada.Shared/Security/Permissions.cs
@@ -0,0 +1,25 @@
+﻿namespace HyperMsg.Scada.Shared.Security;
+
+public static class Permissions
+{
+    public static class Devices
+    {
+        public const string View = "Devices.View";
+        public const string Edit = "Devices.Edit";
+        public const string Delete = "Devices.Delete";
+    }
+
+    public static class DeviceTypes
+    {
+        public const string View = "DeviceTypes.View";
+        public const string Edit = "DeviceTypes.Edit";
+        public const string Delete = "DeviceTypes.Delete";
+    }
+
+    public static class Metrics
+    {
+        public const string View = "Metrics.View";
+        public const string Edit = "Metrics.Edit";
+        public const string Delete = "Metrics.Delete";
+    }
+}

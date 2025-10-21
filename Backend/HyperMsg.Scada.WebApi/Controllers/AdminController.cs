abe88c292d4c92b26278a52e6cda63a6c6e03b53
Max Gunther <maxim.chasnik@gmail.com>
2025-10-21 08:43:15 +0300

    wip

---
+++ Backend/HyperMsg.Scada.WebApi/Controllers/AdminController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HyperMsg.Scada.WebApi.Controllers;
+
+[Authorize(Roles = "Admin")]
+[ApiController]
+[Route("[controller]")]
+public class AdminController : ControllerBase
+{
+    private readonly UserManager<IdentityUser> _userManager;
+
+    public AdminController(UserManager<IdentityUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    #region Users
+
+    [HttpGet("users")]
+    public IActionResult GetUsers()
+    {
+        var users = _userManager.Users.ToList();
+
+        return Ok(users);
+    }
+
+    [HttpPost("user")]
+    public async Task<IActionResult> CreateUser(string userName, string password)
+    {
+        var user = new IdentityUser { UserName = userName, Email = userName };
+        var result = await _userManager.CreateAsync(user, password);
+        if (result.Succeeded)
+        {
+            return Ok("User created successfully.");
+        }
+        else
+        {
+            return BadRequest(result.Errors);
+        }
+    }
+
+    [HttpPut("user/{id}")]
+    public async Task<IActionResult> UpdateUser(string id)
+    {
+        var user = await _userManager.FindByIdAsync(id);
+        if (user == null)
+        {
+            return NotFound("User not found.");
+        }
+        //user.UserName = newUserName;
+        //user.Email = newUserName;
+        var result = await _userManager.UpdateAsync(user);
+        if (result.Succeeded)
+        {
+            return Ok("User updated successfully.");
+        }
+        else
+        {
+            return BadRequest(result.Errors);
+        }
+    }
+
+    #endregion
+
+    #region Permissions
+
+    [HttpPost("user/{id}/permissions")]
+    public async Task<IActionResult> AddPermissionsToUser(string id, [FromBody] List<string> permissions)
+    {
+        var user = await _userManager.FindByIdAsync(id);
+        if (user == null)
+        {
+            return NotFound("User not found.");
+        }
+        var userClaims = await _userManager.GetClaimsAsync(user);
+        foreach (var permission in permissions)
+        {
+            if (!userClaims.Any(c => c.Type == "Permission" && c.Value == permission))
+            {
+                await _userManager.AddClaimAsync(user, new System.Security.Claims.Claim("Permission", permission));
+            }
+        }
+        return Ok("Permissions added successfully.");
+    }
+
+    #endregion
+}
--- Shared/HyperMsg.Scada.Shared/Security/Permissions.cs
+++ Shared/HyperMsg.Scada.Shared/Security/Permissions.cs
@@ -22,4 +22,13 @@ public static class Permissions
         public const string Edit = "Metrics.Edit";
         public const string Delete = "Metrics.Delete";
     }
+
+    public static class Users
+    {
+        public const string Create = "Users.Create";
+        public const string View = "Users.View";
+        public const string Edit = "Users.Edit";
+        public const string Delete = "Users.Delete";
+        public const string AssignPermissions = "Users.AssignPermissions";
+    }
 }

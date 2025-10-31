7f0c17e58ea432a8dd0ec433f21373d695ed423d
Max Gunther <maxim.chasnik@gmail.com>
2025-10-31 14:13:32 +0200

    wip

---
+++ Frontend/HyperMsg.Scada.WebApp/Components/Account/ApplicationUser.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace HyperMsg.Scada.WebApp.Components.Account;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+}
+

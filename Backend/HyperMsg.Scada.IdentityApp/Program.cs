6106fc4b513ab0f93e815a2daf89de01ec5975ea
BuzzX8 <maxim.chasnik@gmail.com>
2025-11-01 14:24:59 +0200

    wip

--- Backend/HyperMsg.Scada.IdentityApp/Program.cs
+++ Backend/HyperMsg.Scada.IdentityApp/Program.cs
@@ -1,9 +1,20 @@
+using HyperMsg.Scada.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var services = builder.Services;
+
+//services.AddDbContext<UserContext>(options =>
+//    options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
